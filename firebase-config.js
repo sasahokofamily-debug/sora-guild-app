10b50ecdb0f7c798ffddafa8508dba1d88dff3df
sasahokofamily-debug <sasahokofamily@gmail.com>
2026-07-01 11:49:12 +0900

    Configure Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,11 @@
 // Firebase Console > プロジェクト設定 > マイアプリ（Web）から取得した値に差し替えてください。
 // 未設定のままではGoogleログインは開始されません。
 window.SORA_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyCDw0NL101DPLXiVeSPuYlp37TpJaURcuM",
+  authDomain: "sora-quest.firebaseapp.com",
+  projectId: "sora-quest",
+  storageBucket: "sora-quest.firebasestorage.app",
+  messagingSenderId: "170485996766",
+  appId: "1:170485996766:web:b6f6021786e0c27072764f",
+  measurementId: "G-GZSQN3DK6S",
 };
